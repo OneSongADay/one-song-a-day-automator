a1acd576399a3c8463dc5de6c05332f70dc46cf8
Paco Lopez <francisco.lopsan@gmail.com>
2022-01-18 23:11:05 +0100

    Reading `res.data.data` and display youtube link

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ const twitterClient = new TwitterClient({
 axios
   .get("https://onesongaday.herokuapp.com/onesongaday")
   .then((res) => {
-    const song = res.data;
-    console.log(res.data);
-    const tweet = `♫ La canción del día ya disponible en #OneSongADay!! Hoy traemos ${song.title} de ${song.author}!!`;
+    const song = res.data.data;
+    const tweet = `♫ La canción del día ya disponible en #OneSongADay!! Hoy traemos ${song.title} de ${song.author}!! https://youtu.be/${song.youtube_link}`;
     twitterClient.tweets
       .statusesUpdate({
         status: tweet,
