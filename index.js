f65f9de60e76143c22baec5bd0b05bee5a4a7352
Jesus L <lopsanjesus@gmail.com>
2022-01-20 22:23:09 +0100

    Comment the whole index to stop cronjob

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const { TwitterClient } = require("twitter-api-client");
-const axios = require("axios");
+// const { TwitterClient } = require("twitter-api-client");
+// const axios = require("axios");
 
-const twitterClient = new TwitterClient({
-  apiKey: process.env.TWITTER_API_KEY,
-  apiSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
+// const twitterClient = new TwitterClient({
+//   apiKey: process.env.TWITTER_API_KEY,
+//   apiSecret: process.env.TWITTER_API_SECRET,
+//   accessToken: process.env.TWITTER_ACCESS_TOKEN,
+//   accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// });
 
-axios
-  .get("https://onesongaday.herokuapp.com/onesongaday")
-  .then((res) => {
-    const song = res.data.data;
-    const tweet = `♫ La canción del día ya disponible en #OneSongADay!! Hoy traemos ${song.title} de ${song.author}!! https://youtu.be/${song.youtube_link}`;
-    twitterClient.tweets
-      .statusesUpdate({
-        status: tweet,
-      })
-      .then((_response) => {
-        console.log(`Successfully tweeted on ${new Date()}`);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// axios
+//   .get("https://onesongaday.herokuapp.com/onesongaday")
+//   .then((res) => {
+//     const song = res.data.data;
+//     const tweet = `♫ La canción del día ya disponible en #OneSongADay!! Hoy traemos ${song.title} de ${song.author}!! https://youtu.be/${song.youtube_link}`;
+//     twitterClient.tweets
+//       .statusesUpdate({
+//         status: tweet,
+//       })
+//       .then((_response) => {
+//         console.log(`Successfully tweeted on ${new Date()}`);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
